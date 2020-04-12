d7a2343e5864e2767a29d94a6214da6254f21733
makikou <ma8618328@gmail.com>
2020-04-12 13:46:34 +0000

    microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
      has_many :followings, through: :relationships, source: :follow
      has_many :likes, through: :favorites, source: :micropost 
      has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
-     has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
      has_many :followers, through: :reverses_of_relationship, source: :user
      
     def follow(other_user)
